e23f50d07d9ff974efac5a793ded819e31d6257e
Septian Adi Wijaya <developadi@gmail.com>
2021-07-02 11:39:22 +0700

    init commit

---
+++ src/main/java/id/niteroomcreation/web_employeeREST/WebEmployeeRestApplication.java
@@ -0,0 +1,13 @@
+package id.niteroomcreation.web_employeeREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEmployeeRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/niteroomcreation/web_employeeREST/WebEmployeeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package id.niteroomcreation.web_employeeREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
